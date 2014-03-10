9d4eff0c9ec7c70a6143d19efee08fc90a114ae3
ipoelhuda <ipoel.huda1@gmail.com>
2014-03-10 13:42:47 +0700

    Update random password.sh

--- random password.sh
+++ random password.sh
@@ -9,7 +9,7 @@ read -p "Masukkan username : " username
 egrep "^$username" /etc/passwd >/dev/null
 if [ $? -eq 0 ]; then
 # proses mengganti passwordnya
-echo -e "$passrandom\n$passrandom" | passwd trial
+echo -e "$passrandom\n$passrandom" | passwd $username
 echo "Password untuk user $username : $passrandom" 
 else
 echo "Username tidak ditemukan"
